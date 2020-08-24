8484e006fdb82fdef4a478711fafdb16e586e968
Delver Villanueva <xdelver_live@outlook.com>
2020-08-24 17:12:55 -0500

    Commit Inicial

---
+++ config/yargs.js
@@ -0,0 +1,22 @@
+const opts = {
+
+    base: {
+        daman: true,
+        alias: 'b'
+    },
+    limite: {
+        alias: 'l',
+        default: 10
+    }
+
+}
+
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de Multiplicar', opts)
+    .command('crear', 'Crea un archivo con  la tabla de Multiplicar', opts)
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
